8c7ebacb83cf65db5a160550ac2d421cbc886a21
answntjs <sendingact@gmail.com>
2025-06-01 18:45:50 +0900

    txt_read_checker 추가함

---
+++ TypePracticing/txt_read_checker.c
@@ -0,0 +1,11 @@
+#include "std.h"
+
+void txt_read_checker(Text* words, Text* sentences){
+	for (int i = 0; i < (*words).length; i++) {
+		printf("%s ", (*words).arr[i]);
+	}
+	printf("\n");
+	for (int i = 0; i < (*sentences).length; i++) {
+		printf("%s\n", (*sentences).arr[i]);
+	}
+}
